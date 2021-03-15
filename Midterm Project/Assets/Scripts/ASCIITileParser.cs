aa50a4d1db661ee8d4840bb8f48124c42500ccac
Alessia <alessia@mipsscan.com>
2021-03-14 20:56:59 -0400

    Ascii comments

--- Midterm Project/Assets/Scripts/ASCIITileParser.cs
+++ Midterm Project/Assets/Scripts/ASCIITileParser.cs
@@ -18,6 +18,7 @@ public class ASCIITileParser : MonoBehaviour
 
     public int goldAMT;
 
+    //keeps track of the player's collected gold! 
     public int GoldAmt
     {
         get
@@ -33,12 +34,17 @@ public class ASCIITileParser : MonoBehaviour
         }
     }
 
+    //UI element to print out gold claimed text!
     public TextMeshProUGUI goldTXT; 
     
+    //I think i probably didn't use this anywhere
     //tile offsets
     public float xOffset;
     public float yOffset;
     
+    //All strings for file names/important files
+    //didn't use the time one I think. 
+    //TODO add in a time tracker for how long the player has played for
     private string FILE_NAME_TERRAIN = "TerrainNum.txt";
     private string FILE_NAME_OBJECTIVES = "ObjectiveNum.txt";
     private const string DIR = "/ASCII/";
@@ -52,8 +58,11 @@ public class ASCIITileParser : MonoBehaviour
     
     private string FILE_PATH_JSON;
 
+    //wherever the player starts the level
     private Vector2 playerStartPos;
 
+    //These are all prefabs used in the tile editor
+    //There are a lot
     [Header("Terrain Prefabs - Sand")] 
     public GameObject sandbarTopLeft; //char 1
     public GameObject sandbarTopMiddle; //char 2
@@ -104,16 +113,22 @@ public class ASCIITileParser : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
+        //I wanted a truly random seed and this was as close as I could get using time in seconds
         Random.seed = System.DateTime.Now.Second;
         
+        //this is the terrain path
+        //this only ended up being one level so the 'Num' doesn't do much now
         PATH_TO_TERRAIN = Application.dataPath + DIR + FILE_NAME_TERRAIN;
         PATH_TO_TERRAIN = PATH_TO_TERRAIN.Replace("Num", levelNum + "");
         
+        //There are 3 possible files with objectives in it, and one of these is loaded at random 
         PATH_TO_OBJECTIVES = Application.dataPath + DIR + FILE_NAME_OBJECTIVES;
         PATH_TO_OBJECTIVES = PATH_TO_OBJECTIVES.Replace("Num", Random.Range(1,3) + "");
 
+        //getting the player's previous gold
         PATH_TO_GOLD = Application.dataPath + DIR_LOGS + FILE_NAME_SAVE_GOLD;
         
+        //player position is extracted from here, if it exists
         FILE_PATH_JSON = Application.dataPath + "/" + name + ".json";
         if (File.Exists(FILE_PATH_JSON))
         {
@@ -122,9 +137,11 @@ public class ASCIITileParser : MonoBehaviour
             playerStartPos = savedPos; 
         }
 
-        
+        //now execute this to get the tiles
         ShoreParser();
-        
+       
+        //get the actual gold value, if it exists
+        //otherwise, just set it to zero. if the player closes with still 0, it won't break (but it does break if null)
         if (!File.Exists(PATH_TO_GOLD))
         {
             File.Create(PATH_TO_GOLD);
@@ -140,14 +157,16 @@ public class ASCIITileParser : MonoBehaviour
 
 
 
-
+//function to get all tiles
     void ShoreParser()
     {
+        //level gameobject to save things under to keep scene cleaner
         GameObject level = new GameObject();
         level.name = "Level";
 
         //terrain
         
+        //first file we look at is for the terrain
         string[] terrain = File.ReadAllLines(PATH_TO_TERRAIN);
 
         for (int y = 0; y < terrain.Length; y++)
@@ -156,20 +175,21 @@ public class ASCIITileParser : MonoBehaviour
             char[] characters = currentLine.ToCharArray(); //we want to get every character of that file
 
             for (int x = 0; x < characters.Length; x++)
-            {
-
+            { 
+//x and y become x and y coords, based on where they are in the ascii file
                 GameObject newObj;
-                char c = characters[x];
+                char c = characters[x]; //the current character
 
+                //long switch statement for the terrain tiles
                 switch (c)
                 {
-                    case 'P': //pirate ship
+                    case 'P': //pirate ship -> this is the player
                         newObj = Instantiate<GameObject>(playerBoatBlue);
                         break;
-                    case 'p': //pirate
+                    case 'p': //pirate -> this is the player on land
                         newObj = Instantiate<GameObject>(pirate);
                         break;
-                    case '1':
+                    case '1': //here down is all land tile types
                         newObj = Instantiate<GameObject>(sandbarTopLeft);
                         break;
                     case '2':
@@ -256,7 +276,7 @@ public class ASCIITileParser : MonoBehaviour
                     case 'z':
                         newObj = Instantiate<GameObject>(grassSandBL);
                         break;
-                    case 'R':
+                    case 'R': //rocks should be one of three type of rock sprite, picked randomly
                         var rock = Random.Range(1, 3);
                         Debug.Log(rock);
                         switch (rock)
@@ -284,13 +304,19 @@ public class ASCIITileParser : MonoBehaviour
 
                 if (newObj != null)
                 {
+                    //this should be the position of most objects
                     newObj.transform.position = new Vector2(x * tileSize + xOffset, -y * tileSize + yOffset);
+                    
+                    //if the object just made is the player ship, set that instead to the last position the player was at when the game was closed
+                    //this only happens if the JSON exists
                     if (newObj.name.Contains("Ship") && File.Exists(FILE_PATH_JSON))
                     {
                         newObj.transform.position = playerStartPos;
                     }
 
                     newObj.GetComponent<SpriteRenderer>().sortingOrder = 0; //set the sort order so these are on the bottom
+                   
+                    //the ship and the pirate should be the top items visisble
                     if (newObj.name.Contains("Ship") || newObj.name.Contains("Pirate"))
                     {
                         newObj.GetComponent<SpriteRenderer>().sortingOrder = 2;
@@ -302,7 +328,7 @@ public class ASCIITileParser : MonoBehaviour
         }
         
         //objectives
-        
+        //we're repeating the above to place treasure markers on land
         string[] treasures = File.ReadAllLines(PATH_TO_OBJECTIVES);
         for (int y = 0; y < treasures.Length; y++)
         {
@@ -314,10 +340,9 @@ public class ASCIITileParser : MonoBehaviour
 
                 GameObject newObj;
                 char c = characters[x];
-
                 switch (c)
                 {
-                    case '#':
+                    case '#': //x marks the spot tiles
                         newObj = Instantiate<GameObject>(gold);
                         break;
                     default:
@@ -328,7 +353,7 @@ public class ASCIITileParser : MonoBehaviour
                 if (newObj != null)
                 {
                     newObj.transform.position = new Vector2(x * tileSize + xOffset, -y * tileSize + yOffset);
-                    newObj.GetComponent<SpriteRenderer>().sortingOrder = 1; //set the sort order so these are on the bottom
+                    newObj.GetComponent<SpriteRenderer>().sortingOrder = 1; //set the sort order so these are above the terrain, but below the player
                     newObj.transform.parent = level.transform;
                 }
                 
@@ -341,6 +366,7 @@ public class ASCIITileParser : MonoBehaviour
     private void OnApplicationQuit()
     {
         Debug.Log("QUIT");
+        //when quitting, save the player's last position in a JSON file
         string jsonPosition = JsonUtility.ToJson(GameObject.FindWithTag("Player").transform.position, true);
         Debug.Log(jsonPosition);
         File.WriteAllText(FILE_PATH_JSON, jsonPosition);
